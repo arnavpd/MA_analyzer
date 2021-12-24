eeb1b67505b56d3a9688d857b5a683f7b9051e26
Arnav Prasad <arnavpd@gmail.com>
2021-12-24 11:43:42 -0800

    Reads data and prints

--- MA_analyzer/Program.cs
+++ MA_analyzer/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int counter = 0;
+
+var lines = System.IO.File.ReadLines(@"..\..\..\data\feed.json");
+
+// Read the file and display it line by line.  
+foreach (string line in lines)
+{
+    System.Console.WriteLine(line);
+    counter++;
+}
+
+System.Console.WriteLine("There were {0} lines.", counter);
+// Suspend the screen.  
+System.Console.ReadLine();
