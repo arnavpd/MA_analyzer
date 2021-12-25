187f4573759dbb375cbad31757b96dea589b1304
Arnav Prasad <arnavpd@gmail.com>
2021-12-25 10:55:24 -0800

    Made extract function which "extracts" 3 key data points, can be used to extract any data we require

--- MA_analyzer/Program.cs
+++ MA_analyzer/Program.cs
@@ -7,10 +7,27 @@ var lines = System.IO.File.ReadLines(@"..\..\..\data\feed.json");
 // Read the file and display it line by line.  
 foreach (string line in lines)
 {
-    System.Console.WriteLine(line);
+    extract(line, "about");
+    extract(line, "team");
+    extract(line, "customers");
+   
     counter++;
 }
 
 System.Console.WriteLine("There were {0} lines.", counter);
 // Suspend the screen.  
-System.Console.ReadLine();
+System.Console.ReadLine();  
+
+string extract (string line, string keyWord)
+{
+    if (line.Contains(keyWord))
+    {
+        var data = line.Substring(line.IndexOf(':') + 1);
+        data = data.Substring(0, data.Length - 1);
+        System.Console.WriteLine(data);
+        System.Console.WriteLine("\n");
+        return data; 
+    }
+    return string.Empty;
+    
+}
